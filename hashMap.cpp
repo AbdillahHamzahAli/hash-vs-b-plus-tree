1eed432b9759bda8fa43b81d9d800c66d6ed0c69
Jaf_Alkarim <149105089+Jafkrim@users.noreply.github.com>
2025-06-27 20:15:50 +0700

    Update hashMap.cpp
    FINISHED

--- hashMap.cpp
+++ hashMap.cpp
@@ -37,24 +37,18 @@ struct Hash {
         return (x % BUCKET);
     }
 
+    int isDuplicate(int x) {
+        int index = hashFunction(x);
+
+        for (auto& item : table[index]) if (x == item.id) return 1;
+        return 0;
+    }
+
     void createItem(node data) {
         int index = hashFunction(data.id);
 
-        for (auto& item : table[index]) {
-            if (data.id == item.id) {
-                char choice;
-
-                cout << "ID " << data.id << " already exists. Do you want to update it? (y/n)" << endl << "> ";
-                cin >> choice;
-
-                choice = tolower(choice);
-
-                if (choice == 'y') { updateItem(data, index); }
-                else { cout << endl << "Item not inserted." << endl << endl; }
-                return;
-            }
-        }
         table[index].push_back(data);
+        return;
     }
 
     void readItem(int key){
@@ -63,14 +57,14 @@ struct Hash {
 
         for (i = table[index].begin(); i != table[index].end(); i++) {
             if ((*i).id == key) {
-                cout << "ID: " << (*i).id << endl;
-                cout << "Nama Produk: " << (*i).nama_produk << endl;
-                cout << "Kategori: " << (*i).kategori << endl;
-                cout << "Harga: Rp" << fixed << setprecision(2) << (*i).harga << endl;
-                cout << "Stok: " << (*i).stok << endl;
-                cout << "Berat: " << (*i).berat_gram << " gr" << endl;
-                cout << "Deskripsi: " << (*i).deskripsi << endl;
-                cout << "Tersedia: " << (((*i).tersedia) ? "Ya" : "Tidak") << endl << endl;
+                cout << "ID           : " << (*i).id << endl;
+                cout << "Product Name : " << (*i).nama_produk << endl;
+                cout << "Category     : " << (*i).kategori << endl;
+                cout << "Price        : Rp" << fixed << setprecision(2) << (*i).harga << endl;
+                cout << "Stock        : " << (*i).stok << endl;
+                cout << "Weight       : " << (*i).berat_gram << " gr" << endl;
+                cout << "Description  : " << (*i).deskripsi << endl;
+                cout << "Available    : " << (((*i).tersedia) ? "Ya" : "Tidak") << endl << endl;
                 return;
             }
         }
@@ -81,6 +75,7 @@ struct Hash {
     void updateItem(node data, int key) {
         deleteItem(key);
         createItem(data);
+        return;
     }
 
     void deleteItem(int key){
@@ -91,7 +86,9 @@ struct Hash {
             if ((*i).id == key) break;
         }
         if (i != table[index].end()) table[index].erase(i);
+        else cout << "ID " << (*i).id << " doesn't exist." << endl;
         cout << endl;
+        return;
     }
 };
 template<typename bench>
@@ -106,6 +103,7 @@ void chronoBench(bench start, bench end, int totalData, string method) {
     cout << "| Total time : " << setw(9) << timeElapsed.count() << " ms" << setw(7) << "|" << endl;
     cout << "| Throughput : " << setw(9) << static_cast<int>(floor(opsPerSec)) << " ops/sec" << setw(2) << "|" << endl;
     cout << "----------------------------------" << endl << endl;
+    return;
 }
 
 void hashCreate(Hash* h, int totalData) {
@@ -119,41 +117,61 @@ void hashCreate(Hash* h, int totalData) {
     bool tersedia;
     node add;
 
-    cout << "ID (bulat)      : ";
+    cout << "Enter the ID (integer) : ";
     cin >> id;
     add.id = id;
-    cout << "Nama Produk     : ";
+
+    if (h->isDuplicate(add.id)) {
+        char choice;
+
+        cout << endl << "ID " << add.id << " already exists. Do you want to update it? (y/n)" << endl << "> ";
+        cin >> choice;
+
+        choice = tolower(choice);
+
+        if (choice != 'y') {
+            cout << endl << "Item not inserted." << endl << endl;
+            return;
+        }
+    }
+    cout << endl << "Product Name    : ";
     cin >> nama_produk;
     add.nama_produk = nama_produk;
-    cout << "Kategori        : ";
+    cout << "Category        : ";
     cin >> kategori;
     add.kategori = kategori;
-    cout << "Harga (desimal) : ";
+    cout << "Price (decimal) : ";
     cin >> harga;
     add.harga = harga;
-    cout << "Stok (bulat)    : ";
+    cout << "Stock (integer) : ";
     cin >> stok;
     add.stok = stok;
-    cout << "Berat (gram)    : ";
+    cout << "Weight (gram)   : ";
     cin >> berat_gram;
     add.berat_gram = berat_gram;
-    cout << "Deskripsi       : ";
+    cout << "Description     : ";
     cin >> deskripsi;
     add.deskripsi = deskripsi;
-    cout << "Tersedia (0/1)  : ";
+    cout << "Available (0/1) : ";
     cin >> tersedia;
     cout << endl;
     add.tersedia = tersedia;
 
     auto benchStart = chrono::high_resolution_clock::now();
-    h->createItem(add);
+
+    if (h->isDuplicate(id)) h->updateItem(add, h->hashFunction(add.id));
+    else h->createItem(add);
+    
     auto benchEnd = chrono::high_resolution_clock::now();
     chronoBench(benchStart, benchEnd, totalData, "hash-map");
+    return;
 }
 
 void hashRead(Hash* h, int totalData) {
     char searchMode;
-    int id;
+    int id,
+        startId,
+        endId;
     
     cout << "CHOOSE SEARCH MODE:" << endl
          << "(1) Single data" << endl
@@ -161,18 +179,28 @@ void hashRead(Hash* h, int totalData) {
          << "(other) cancel" << endl
          << "> ";
     cin >> searchMode;
+    cout << endl;
 
     if (searchMode == '1') {
+        cout << "Enter the ID : ";
         cin >> id;
+        cout << endl;
 
         auto benchStart = chrono::high_resolution_clock::now();
         h->readItem(id);
         auto benchEnd = chrono::high_resolution_clock::now();
         chronoBench(benchStart, benchEnd, totalData, "hash-map");
     } else if (searchMode == '2') {
-        // NOT YET DONE
+        cout << "Enter start ID : ";
+        cin >> startId;
+        cout << "Enter end ID   : ";
+        cin >> endId;
+        cout << endl;
+
         auto benchStart = chrono::high_resolution_clock::now();
-        
+
+        for (int temp_id = startId; temp_id <= endId; temp_id++) h->readItem(temp_id);
+
         auto benchEnd = chrono::high_resolution_clock::now();
         chronoBench(benchStart, benchEnd, totalData, "hash-map");
     }
@@ -180,17 +208,80 @@ void hashRead(Hash* h, int totalData) {
 }
 
 void hashUpdate(Hash* h, int totalData) {
-    auto benchStart = chrono::high_resolution_clock::now();
-    
-    auto benchEnd = chrono::high_resolution_clock::now();
-    chronoBench(benchStart, benchEnd, totalData, "hash-map");
+    int id,
+        stok,
+        berat_gram;
+    float harga;
+    string nama_produk,
+           kategori,
+           deskripsi;
+    bool tersedia;
+    node update;
+
+    cout << "Enter the ID (integer) : ";
+    cin >> id;
+    update.id = id;
+    cout << endl;
+
+    if (h->isDuplicate(update.id)) {
+        cout << "Product Name    : ";
+        cin >> nama_produk;
+        update.nama_produk = nama_produk;
+        cout << "Category        : ";
+        cin >> kategori;
+        update.kategori = kategori;
+        cout << "Price (decimal) : ";
+        cin >> harga;
+        update.harga = harga;
+        cout << "Stock (integer) : ";
+        cin >> stok;
+        update.stok = stok;
+        cout << "Weight (gram)   : ";
+        cin >> berat_gram;
+        update.berat_gram = berat_gram;
+        cout << "Description     : ";
+        cin >> deskripsi;
+        update.deskripsi = deskripsi;
+        cout << "Available (0/1) : ";
+        cin >> tersedia;
+        cout << endl;
+        update.tersedia = tersedia;
+
+        auto benchStart = chrono::high_resolution_clock::now();
+        h->updateItem(update, h->hashFunction(update.id));
+        auto benchEnd = chrono::high_resolution_clock::now();
+        chronoBench(benchStart, benchEnd, totalData, "hash-map");
+        return;
+    }
+    cout << "ID " << update.id << " doesn't exist." << endl << endl;
+    return;
 }
 
 void hashDelete(Hash* h, int totalData) {
-    auto benchStart = chrono::high_resolution_clock::now();
-    
-    auto benchEnd = chrono::high_resolution_clock::now();
-    chronoBench(benchStart, benchEnd, totalData, "hash-map");
+    int id;
+    char choice;
+
+    cout << "Enter the ID (integer) : ";
+    cin >> id;
+    cout << endl << "===============" << endl << endl;
+
+    h->readItem(id);
+
+    cout << "===============" << endl << "Are you sure to delete all the data above? (y/n)" << endl << "> ";
+    cin >> choice;
+    cout << endl;
+
+    choice = tolower(choice);
+
+    if ((h->isDuplicate(id)) && (choice == 'y')) {
+        auto benchStart = chrono::high_resolution_clock::now();
+        h->deleteItem(id);
+        auto benchEnd = chrono::high_resolution_clock::now();
+        chronoBench(benchStart, benchEnd, totalData, "hash-map");
+        return;
+    }
+    cout << "Revoking the deletion operation." << endl << endl;
+    return;
 }
 
 int main() {
@@ -267,7 +358,7 @@ int main() {
             else if (chooseCase == '3') hashUpdate(h, data.size());
             else if (chooseCase == '4') hashDelete(h, data.size());
             else {
-                cout << "No CRUD operation running, workflow is now closed." << endl << endl;
+                cout << "No CRUD operations are running, the workflow is now closed." << endl << endl;
                 file.close();
             }
         } while ((chooseCase == '1') || (chooseCase == '2') || (chooseCase == '3') || (chooseCase == '4'));
